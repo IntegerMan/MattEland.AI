d8dabf94e56519850275c04edc00063e21df72bb
Matt Eland <Matt.Eland@GMail.com>
2019-04-14 14:55:17 -0400

    Cleaned up helper code.

---
+++ MattEland.Shared/Collections/CollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MattEland.Shared.Collections
+{
+    /// <summary>
+    /// Helper extension methods for working with collections or ranges.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Performs an operation on each member of the collection and returns the source collection
+        /// </summary>
+        /// <typeparam name="T">The type contained in the collection</typeparam>
+        /// <param name="source">The collection</param>
+        /// <param name="operation">The operation to perform on each member of the collection</param>
+        /// <returns>The <paramref name="source"/> collection.</returns>
+        public static IEnumerable<T> Each<T>(this IEnumerable<T> source, Action<T> operation)
+        {
+            if (operation == null) throw new ArgumentNullException(nameof(operation));
+
+            if (source != null)
+            {
+                foreach (var item in source)
+                {
+                    operation(item);
+                }
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Performs an operation with each number from 0 to <paramref name="times"/>.
+        /// </summary>
+        /// <param name="times">The amount of times the operation should be invoked</param>
+        /// <param name="operation">The operation to perform</param>
+        public static void Each(this int times, Action<int> operation)
+        {
+            Enumerable.Range(0, times).Each(operation);
+        }
+    }
+}
---
+++ MattEland.Shared/Strings/IStringBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace MattEland.Shared.Strings
+{
+    /// <summary>
+    /// An interface for a string builder wrapper.
+    /// </summary>
+    /// <remarks>
+    /// This is a bit cleaner than passing IndentingStringBuilder around everywhere, but I still don't love it.
+    /// </remarks>
+    public interface IStringBuilder
+    {
+        /// <summary>
+        /// Indents the display, causing future lines to be indented.
+        /// </summary>
+        [UsedImplicitly]
+        void Indent();
+
+        /// <summary>
+        /// Outdents the display, causing future lines to be less indented.
+        /// </summary>
+        [UsedImplicitly]
+        void Outdent();
+
+        /// <summary>
+        /// Appends an empty line.
+        /// </summary>
+        void AppendLine();
+
+        /// <summary>
+        /// Appends a line to the display, respecting the current indentation level
+        /// </summary>
+        void AppendLine(string message);
+
+        /// <summary>
+        /// Creates an IDisposable scope that will invoke Indent on enter and Outdent on dispose.
+        /// </summary>
+        /// <returns>An IDisposable indentation scope, intended to be stored in a using Statement</returns>
+        IDisposable IndentScope();
+    }
+}
