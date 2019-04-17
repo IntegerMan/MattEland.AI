6915513a6caa4f4bba774613122ed801a70aa1f1
Matt Eland <Matt.Eland@GMail.com>
2019-04-16 21:24:11 -0400

    Beginning work on an F# version

---
+++ MattEland.AI.Neural.Functional/Neuron.fs
@@ -0,0 +1,28 @@
+﻿namespace MattEland.AI.Neural.Functional
+
+/// Represents a connection between two Neurons
+type NeuronConnection(source: Neuron, weight: decimal) = 
+  member this.Source: Neuron = source;
+  member this.Weight: decimal = weight;
+  member this.Calculate = this.Weight * this.Source.Value;
+
+/// Represents a node in a Neural Network
+and Neuron () =
+
+  let mutable connections = []
+  let mutable value = 0M;
+
+  /// Incoming connections from other Neurons (if any)
+  member this.Inputs = connections
+
+  /// Exposes the current calculated amount of the Neuron
+  member this.Value = value;
+  
+  /// Adds an incoming connection from another Neuron
+  member this.AddIncomingConnection c = connections <- c :: connections
+
+  /// Adds all items in the collection together
+  member this.Evaluate (numbers: decimal list) = 
+    List.iter (fun (c:NeuronConnection) -> (value <- value + c.Calculate)) this.Inputs;
+
+  
---
+++ XUnitTestProject1/FunctionalTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MattEland.AI.Neural.Functional;
+using Microsoft.FSharp.Collections;
+using Shouldly;
+using Xunit;
+
+namespace MattEland.AI.Tests
+{
+    public class FunctionalTests
+    {
+
+        [Fact]
+        public void AddConnectionRegistersConnection()
+        {
+            // Arrange
+            var neuron = new Neuron();
+            var connection = new NeuronConnection(neuron, 1m);
+
+            // Act
+            neuron.AddIncomingConnection(connection);
+
+            // Assert
+            neuron.Inputs.Length.ShouldBe(1);
+            neuron.Inputs[0].ShouldBe(connection);
+        }
+    }
+}
